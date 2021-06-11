3945075af368c26a82b63f929301ab0faeaf2766
Chris Free <chris@chromatichq.com>
2021-06-11 11:58:54 -0500

    Initial Commit

---
+++ materialPlayground/materialPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  materialPlaygroundApp.swift
+//  materialPlayground
+//
+//  Created by Chris Free on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct materialPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
